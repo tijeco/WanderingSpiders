f2ef61e0f3df25826abe9cd7a5456c4ddf24b7d6
Jeff <jffcole7@gmail.com>
2018-12-09 10:32:01 -0500

    this seems to work for the most part!

--- spider/spider.go
+++ spider/spider.go
@@ -1,9 +1,8 @@
 package spider
 
 import (
-	"fmt"
 	"math/rand"
-	"strconv"
+	"runtime"
 	"time"
 )
 
@@ -11,6 +10,10 @@ type Spider struct {
 	Mass float64
 	Cals float64
 }
+
+// const (
+// 	NGoRoutines = 4
+// )
 
 func AddToMap(key string, val Spider, c chan map[string]Spider) {
 	mappy := make(map[string]Spider)
@@ -26,29 +29,60 @@ var spiderMass, spiderCals, num float64
 var spiderID string
 var currentSpider Spider
 
-func MakeSpiders(n int) {
+func MakeSpiders(n int, NGoRoutines int) []Spider {
+	array := make([]Spider, n)
+	c := make(chan int, NGoRoutines)
+	runtime.GOMAXPROCS(NGoRoutines)
 
-	// var spiders [n]Spider
-	spiders := make([]map[string]Spider, n)
-	c := make(chan map[string]Spider)
+	chunk := len(array) / NGoRoutines
 
-	go func() {
-		for spiderNum := 0; spiderNum < n; spiderNum++ {
-			rand.Seed(time.Now().UnixNano())
-			num = rand.Float64()
-			spiderMass = spiderMassMin + num*(spiderMassMax-spiderMassMin)
-			spiderCals = calPerDayMin + num*(calPerDayMax-calPerDayMin)
-			spiderID = "spider_" + strconv.Itoa(n)
-			fmt.Println(spiderID)
-			currentSpider = Spider{Mass: spiderMass, Cals: spiderCals}
-			AddToMap(spiderID, currentSpider, c)
-		}
-		close(c)
+	for i := 0; i < NGoRoutines; i++ {
+		go func(start int) {
+			end := start + chunk
 
-	}()
-	for ret := range c {
-		spiders = append(spiders, ret)
+			if end > len(array) {
+				end = len(array)
+			}
+
+			for j := start; j < end; j = j + 1 {
+				rand.Seed(time.Now().UnixNano())
+				num = rand.Float64()
+				spiderMass = spiderMassMin + num*(spiderMassMax-spiderMassMin)
+				spiderCals = calPerDayMin + num*(calPerDayMax-calPerDayMin)
+
+				array[j].Mass = spiderMass
+				array[j].Cals = spiderCals
+			}
+			c <- 1
+		}(i * chunk)
 	}
-	fmt.Println(spiders)
 
+	// wait for goroutines to finish
+	for i := 0; i < NGoRoutines; i++ {
+		<-c
+	}
+	//
+	// // var spiders [n]Spider
+	// spiders := make([]map[string]Spider, n)
+	// c := make(chan map[string]Spider)
+	//
+	// go func() {
+	// 	for spiderNum := 0; spiderNum < n; spiderNum++ {
+	// 		rand.Seed(time.Now().UnixNano())
+	// 		num = rand.Float64()
+	// 		spiderMass = spiderMassMin + num*(spiderMassMax-spiderMassMin)
+	// 		spiderCals = calPerDayMin + num*(calPerDayMax-calPerDayMin)
+	// 		spiderID = "spider_" + strconv.Itoa(n)
+	// 		fmt.Println(spiderID)
+	// 		currentSpider = Spider{Mass: spiderMass, Cals: spiderCals}
+	// 		AddToMap(spiderID, currentSpider, c)
+	// 	}
+	// 	close(c)
+	//
+	// }()
+	// for ret := range c {
+	// 	spiders = append(spiders, ret)
+	// }
+	// fmt.Println(spiders)
+	return array
 }
